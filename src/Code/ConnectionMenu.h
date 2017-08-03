2d0e4d00523e1409c52e5991b63cc037436fcebd
LightTab2 <azorro12@gmail.com>
2017-08-03 17:24:07 +0200

    Fixed copy-control members for ConnectionMenu

--- src/Code/ConnectionMenu.h
+++ src/Code/ConnectionMenu.h
@@ -6,11 +6,14 @@
 class ConnectionMenu
 {
 public:
-	ConnectionMenu();
+	friend void swap(ConnectionMenu &a, ConnectionMenu &b);
+	ConnectionMenu() {};
+	ConnectionMenu(const ConnectionMenu &copy) { *this = copy; };
+	ConnectionMenu(ConnectionMenu &&copy) { *this = copy; };
 	ConnectionMenu(std::shared_ptr<sf::TcpSocket> socket, std::shared_ptr<std::wofstream> ptr, std::wstring address = L"", std::wstring username = L"", std::wstring password = L"");
 	~ConnectionMenu();																//Nie jestem pewien czy nie trzeba siê roz³¹czyæ
-	ConnectionMenu& operator=(ConnectionMenu) &;
-	ConnectionMenu& operator=(ConnectionMenu&&) &;
+	ConnectionMenu& operator=(const ConnectionMenu& assg) &;
+	ConnectionMenu& operator=(ConnectionMenu&& assg) &;
 	void Connect();																	//Myœla³em o prze³adowaniu jakiegoœ operatora
 private:
 	std::shared_ptr<sf::TcpSocket> socket;
